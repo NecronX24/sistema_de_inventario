d069da67b72b00077e14cba382c4225ebae8918c
Sebangel <angeljavicueche@gmail.com>
2023-07-09 19:45:48 -0400

    Se cambio una funcion al archivo Buscar.h

--- Editar.h
+++ Editar.h
@@ -75,27 +75,3 @@ void editarFun(int idprod){
         break;
     }
 }
-
-int partion_id(inventario arr[], int low, int high){
-  inventario pivote = arr[high];
-  int i = low-1;
-  int j=0;
-  for (j=low; j<=high-1; j++){
-    int num = stoi(arr[j].id);
-    int pivote_int = stoi(pivote.id);
-    if(num<=pivote_int){
-      i++;
-      swap(arr[i],arr[j]);
-    }
-  }
-  swap(arr[i+1],arr[j]);
-  return (i+1);
-}
-
-void quicksort_id(inventario arr[], int low, int high){
-  if (low<high){
-    int pi=partion_id(arr,low,high);
-    quicksort_id(arr,low,pi-1);
-    quicksort_id(arr,pi+1,high);
-  }
-}
