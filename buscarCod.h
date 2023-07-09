49a67e49d0be2c63dff8eb107c4f39ce4635877c
Sebangel <angeljavicueche@gmail.com>
2023-07-09 19:56:07 -0400

    Se intercambian las funciones que estaban en los archivos

--- buscarCod.h
+++ buscarCod.h
@@ -4,7 +4,7 @@
 
 void buscarCodFun(){
     int idprod;
-    cout<<"ingrese ID del producto a buscar:";
+    cout<<"ingrese ID del producto a mostrar:";
     cin>>idprod;
 
     idprod=idprod-1;
@@ -13,4 +13,15 @@ void buscarCodFun(){
 
     cout<<"2-Precio:------"<<datos_inv[idprod].precio<<endl;
 
+    cout<<"3-Existencia:------"<<datos_inv[idprod].existencia<<endl;
+
+    cout<<"4-Maximo:------"<<datos_inv[idprod].max<<endl;
+
+    cout<<"5-Minimo:------"<<datos_inv[idprod].min<<endl;
+
+    cout<<"6-Ventas al dia:------"<<datos_inv[idprod].ventas_dia<<endl;
+
+    cout<<"7-Ventas al mes:------"<<datos_inv[idprod].ventas_mes<<endl;
+
+    cout<<"8-Ventas al ano:------"<<datos_inv[idprod].ventas_ano<<endl;
 }
--- mostrar.h
+++ mostrar.h
@@ -4,24 +4,11 @@
 
 void mostrarFun(){
     int idprod;
-    cout<<"ingrese ID del producto a mostrar:";
+    cout<<"ingrese ID del producto a buscar:";
     cin>>idprod;
 
     idprod=idprod-1;
 
-    cout<<" 1-Nombre:------"<<datos_inv[idprod].nombre<<endl;
+    cout<<"El producto que buscas es "<<datos_inv[idprod].nombre<<" y tiene un costo de "<<datos_inv[idprod].precio<<endl;
 
-    cout<<"2-Precio:------"<<datos_inv[idprod].precio<<endl;
-
-    cout<<"3-Existencia:------"<<datos_inv[idprod].existencia<<endl;
-
-    cout<<"4-Maximo:------"<<datos_inv[idprod].max<<endl;
-
-    cout<<"5-Minimo:------"<<datos_inv[idprod].min<<endl;
-
-    cout<<"6-Ventas al dia:------"<<datos_inv[idprod].ventas_dia<<endl;
-
-    cout<<"7-Ventas al mes:------"<<datos_inv[idprod].ventas_mes<<endl;
-
-    cout<<"8-Ventas al ano:------"<<datos_inv[idprod].ventas_ano<<endl;
 }
