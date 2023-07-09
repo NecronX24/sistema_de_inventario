a11773b4aa7fc5056436c10df0bb6607e5000a88
Carlos <carloshightower0502@gmail.com>
2023-07-09 02:58:02 -0400

    Funcion mostrar realizada

---
+++ mostrar.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include "struct.h"
+
+void mostrarFun(int idprod){
+
+    cout<<"ingrese ID del producto a mostrar:";
+    cin>>idprod;
+
+    idprod=idprod-1;
+
+    cout<<" 1-Nombre:------"<<datos_inv[idprod].nombre<<endl;
+
+    cout<<"2-Precio:------"<<datos_inv[idprod].precio<<endl;
+
+    cout<<"3-Existencia:------"<<datos_inv[idprod].existencia<<endl;
+
+    cout<<"4-Maximo:------"<<datos_inv[idprod].max<<endl;
+
+    cout<<"5-Minimo:------"<<datos_inv[idprod].min<<endl;
+
+    cout<<"6-Ventas al dia:------"<<datos_inv[idprod].ventas_dia<<endl;
+
+    cout<<"7-Ventas al mes:------"<<datos_inv[idprod].ventas_mes<<endl;
+
+    cout<<"8-Ventas al ano:------"<<datos_inv[idprod].ventas_ano<<endl;
+}
