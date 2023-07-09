74e2699b4ca9269b336737e412ac2c0be4934682
Sebangel <angeljavicueche@gmail.com>
2023-07-08 22:32:29 -0400

    se modifica struct para utilizarlo en string

--- struct.h
+++ struct.h
@@ -3,6 +3,5 @@
 using namespace std;
 
 struct inventario{
-string id, nombre, precio, max, min, ventas_dia, ventas_mes, ventas_ano;
-int existencia;
+string nombre, precio, id, existencia,max, min, ventas_dia, ventas_mes, ventas_ano;
 }datos_inv[10000];
