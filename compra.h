b9fc9fe699aab370c519625de52c19b7b7f43cc7
Sebangel <angeljavicueche@gmail.com>
2023-07-09 21:35:38 -0400

    Se arreglan detalles visuales y se agrega un if else ppara casos donde se quiera comprar mas productos de los que se tienen

--- compra.h
+++ compra.h
@@ -4,40 +4,47 @@
 #include "struct.h"
 
 void compraFun(int idcomp){
-    cout<<"Inserta el id del producto que quieres comprar:"<<endl;
+    cout<<"Inserta el ID del producto que quieres comprar"<<endl;
+    cout<<">> ";
     cin>>idcomp;
     idcomp=idcomp-1;
 
     int cantcomp;
     cout<<"Que cantidad deseas comprar de este producto?"<<endl;
+    cout<<">> ";
     cin>>cantcomp;
-//Suma de la compra
-
-    int compAno;
-    compAno=stoi(datos_inv[idcomp].ventas_ano);
-    compAno=compAno+cantcomp;
-
-    int compDia;
-    compDia=stoi(datos_inv[idcomp].ventas_dia);
-    compDia=compDia+cantcomp;
-
-    int compMes;
-    compMes=stoi(datos_inv[idcomp].ventas_mes);
-    compMes=compMes+cantcomp;
-
-//Resta de existencia
-
     int compEx;
     compEx=stoi(datos_inv[idcomp].existencia);
-    compEx=compEx-cantcomp;
+    if (cantcomp<=compEx){
+        //Suma de la compra
 
-//Pasar ints a string
+        int compAno;
+        compAno=stoi(datos_inv[idcomp].ventas_ano);
+        compAno=compAno+cantcomp;
 
-datos_inv[idcomp].ventas_ano=to_string(compAno);
-datos_inv[idcomp].ventas_dia=to_string(compDia);
-datos_inv[idcomp].ventas_mes=to_string(compMes);
-datos_inv[idcomp].existencia=to_string(compEx);
+        int compDia;
+        compDia=stoi(datos_inv[idcomp].ventas_dia);
+        compDia=compDia+cantcomp;
 
-cout<<"Compra hecha exitosamente!";
+        int compMes;
+        compMes=stoi(datos_inv[idcomp].ventas_mes);
+        compMes=compMes+cantcomp;
+
+        //Resta de existencia
+
+        compEx=compEx-cantcomp;
+
+        //Pasar ints a string
+
+        datos_inv[idcomp].ventas_ano=to_string(compAno);
+        datos_inv[idcomp].ventas_dia=to_string(compDia);
+        datos_inv[idcomp].ventas_mes=to_string(compMes);
+        datos_inv[idcomp].existencia=to_string(compEx);
+
+        cout<<"Compra hecha exitosamente!"<<endl;
+    }
+    else{
+        cout<<"No se puede realizar la compra porque no hay suficientes existencias"<<endl;
+    }
   
 }
